8522e53a3cfa731b93627f1687278482313cd3c4
Swen Nijboer <swen-nijboer@hotmail.com>
2018-04-18 18:13:58 +0200

    some comments'

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,22 +1,24 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.action == "getSource") {
-        message.innerText = request.source;
-    }
-});
-
 function onWindowLoad() {
 
+    // We set a var to access the inner content of the message div on the main extension page
     var message = document.querySelector('#message');
 
+    // Params (tab id, script to inject, callback funcion)
     chrome.tabs.executeScript(null, {
         file: "getPagesSource.js"
     }, function() {
+        // Is called after the script has been executed
         // If you try and inject into an extensions page or the webstore/NTP you'll get an error
         if (chrome.runtime.lastError) {
             message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
         }
     });
-
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+        message.innerText = request.source;
+    }
+});
 
 window.onload = onWindowLoad;
