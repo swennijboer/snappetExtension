9c1b7cdee00366b8745988df2782858aa58f7765
Swen Nijboer <swen-nijboer@hotmail.com>
2018-04-19 09:27:47 +0200

    plan de campagne added

--- Extension/popup.js
+++ Extension/popup.js
@@ -15,9 +15,25 @@ function onWindowLoad() {
     });
 }
 
+// Event listener, when we send a message we want this function to receive it and handle it
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
-        message.innerText = request.source;
+
+        // We now have the full source of the page
+        var source = request.source;
+
+        // Lets get to the interesting part
+        var data = source.split('var options');
+
+        // Within segment we will search for pupils, once pupils is found, take everything behind it between { and } (is correct JSON)
+        var segment = data[1];
+        var pupils = "Get DEM PUPILS";
+
+        // Continue where the last reader stopped, and search for progressPerPupil, here { } is used within so keep a counter of number of opened and closed ones
+
+
+        // Can be removed later, we will generate a PDF and open it in a new tab. For now easy for debugging
+        message.innerText = source;
     }
 });
 
